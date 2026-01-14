0d9d0d8ca1fd6d9b2b7d3e418c2ec2880e093b95
Alfin <mohammad.allfinnn@gmail.com>
2026-01-14 10:30:24 +0700

    change bootstrap.js

--- backend/server/bootstrap.js
+++ backend/server/bootstrap.js
@@ -1,15 +1,2 @@
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({
-  path: path.join(__dirname, "..", ".env"),
-});
-
-console.log("PG_USER =", process.env.PG_USER);
-console.log("PG_PASSWORD =", process.env.PG_PASSWORD ? "[ADA]" : "[KOSONG]");
-
+// server/bootstrap.js
 import "./index.js";
